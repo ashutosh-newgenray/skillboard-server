0821bccc653f2c8d72bd241ea4359144ddffb24a
NAVDEEP SINGH <navsingh15@publicisgroupe.net>
2021-03-02 00:57:10 +0530

    updated feedback route

--- routes/feedback.js
+++ routes/feedback.js
@@ -2,20 +2,33 @@ var express = require("express");
 var router = express.Router();
 var ObjectId = require("mongodb").ObjectID;
 
-// /* GET All schedular jobs. */
-// router.get("/", async (req, res, next) => {
-//   const resp = await req.app.DB.collection("schedular_jobs").find({}).toArray();
-//   res.json(resp);
-// });
-
-/* GET questions by skill.*/
+//save profile data from feedback
 router.post("/save", async (req, res, next) => {
-  const resp = await req.app.DB.collection("profile")
-    .find({
-      skill: req.params.skill,
-    })
-    .toArray();
-  res.json(resp);
+  try {
+    const jobData = req.body.jobData;
+    const feedbackFrom = jobData.feedback_from.filter(
+      (item) => item._id.toString() === req.body.userId
+    )[0].emp_id;
+    await req.app.DB.collection("profiles").updateOne(
+      {
+        emp_id: jobData.feedback_for.emp_id,
+      },
+      {
+        $push: {
+          feedback: {
+            emp_id: feedbackFrom,
+            skill: jobData.skill_code,
+            job_id: jobData._id,
+            score: parseInt(req.body.payload["q-0"]),
+            received_at: new Date().toString(),
+          },
+        },
+      }
+    );
+    res.json({ status: 200, message: "profile updated" });
+  } catch (err) {
+    res.json({ error: err });
+  }
 });
 
 module.exports = router;
